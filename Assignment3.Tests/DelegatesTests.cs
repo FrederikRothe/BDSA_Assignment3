41b29cff6b990c15a7191de82d262ed2755fc7b9
Thomas <thwr@itu.dk>
2021-09-27 16:31:36 +0200

    implement reverseString Test

--- Assignment3.Tests/DelegatesTests.cs
+++ Assignment3.Tests/DelegatesTests.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using Assignment3;
 
 namespace BDSA2021.Assignment03.Tests
 {
     public class DelegatesTests
     {
         [Fact]
-        public void Test1()
+        public void reverseString_given_Hej_return_jeh()
         {
+            //arrange
+            var input = "hej";
 
+            //act
+            var output = Program.reverseString(input);
+
+            //assert
+            Assert.Equal("jeh", output);
         }
     }
 }
--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace Assignment3
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
