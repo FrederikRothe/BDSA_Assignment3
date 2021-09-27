1978a5f9c75e9077c6db9c4c038b82f52c597db5
Frakty <66801011+FrederikRothe@users.noreply.github.com>
2021-09-27 14:56:42 +0200

    First extension.

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BDSA2020.Assignment03
 {
     public static class Extensions
     {
+       
+       public static IEnumerable<int> flatten(IEnumerable<int>[] xs)
+       {
+           return(xs.SelectMany(c => c));
+       }
     }
 }
