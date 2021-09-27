43af7de176226d5af0d24b4a0bb1f52a12d21f3d
ThomasITU <75098556+ThomasITU@users.noreply.github.com>
2021-09-27 17:40:04 +0200

    Delegates (#2)
    * First extension.
    
    * Namespace fix, and first two extension methods.
    
    * Implent flatten test
    
    * Implemented last Extension method.
    
    * Implement divisibleBy7 test
    
    * Implement leapYears Test
    
    * Last two extension methods.
    
    * Implement Uri test
    
    * Implement workCount test
    
    * Namespace fix.
    
    * reverseString().
    
    * implement reverseString Test
    
    * implement multiplyDecimal, and multiplyDecimal test
    
    * Implement numericallyEqually and test
    
    Co-authored-by: Frakty <66801011+FrederikRothe@users.noreply.github.com>

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Linq;
 
 namespace Assignment3
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        public static string reverseString(string s)
+        {
+            Func<string,string> reverseString = default(Func<string,string>);
+            
+             reverseString = s => new string(s.Reverse().ToArray());
+
+             return reverseString(s);
+        }
+
+        public static double multiplyDecimals(double x, double y)
+        {
+            Func<double, double, double> product = default(Func<double,double, double>);
+            
+            product = (x, y) => x * y;
+
+            return product(x, y);
+        }
+
+        public static bool numericallyEqually(string x, double y)
+        {
+            Func<string, double, bool> equallity = default(Func<string, double, bool>);
+
+            equallity = (x,y) => double.Parse(x) == y;
+
+            return equallity(x,y);
         }
     }
 }
