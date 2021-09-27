b31e75d5c433d42c86a67497d74aabdb4165ea45
Frederik Rothe <66801011+FrederikRothe@users.noreply.github.com>
2021-09-27 16:05:43 +0200

    Extension methods(). (#1)
    * First extension.
    
    * Namespace fix, and first two extension methods.
    
    * Implent flatten test
    
    * Implemented last Extension method.
    
    * Implement divisibleBy7 test
    
    * Implement leapYears Test
    
    * Last two extension methods.
    
    * Implement Uri test
    
    * Implement workCount test
    
    Co-authored-by: Thomas <thwr@itu.dk>

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,14 +1,75 @@
 using System;
 using Xunit;
+using BDSA2021.Assignment03;
 
-namespace BDSA2020.Assignment03.Tests
+namespace BDSA2021.Assignment03.Tests
 {
     public class ExtensionsTests
     {
         [Fact]
-        public void Test1()
-        {
+        public void given_Nested_Array_of_ints_Flatten_Array()
+        {   
+            //arrange
+            var input = new [] {new int[] {1,2,3,4}, new int[] {5,6,7,8}};
 
+            //act
+            var output = Extensions.flatten(input);
+
+            //assert
+            Assert.Equal(new int[]{1,2,3,4,5,6,7,8}, output);
+        }
+
+        [Fact]
+        public void given_An_Array_of_ints_only_return_a_56_63_70()
+        {
+            //arrange
+            var input = new int[] {7,8,42,55,56,63,64,70};
+
+            //act
+            var output = Extensions.numbersDivisbleBy7AndGreaterThan42(input);
+
+            //assert
+
+            Assert.Equal(new int[]{56,63,70}, output);
+        } 
+
+        [Fact]
+        public void given_array_of_ints_leapYears_return_40_400_1600_2000()
+        {
+            //arrange
+            var input = new int[] {40, 400, 100, 102, 1600, 2000, 1900};
+
+            //act
+            var output = Extensions.findLeapYears(input);
+
+            //assert
+            Assert.Equal(new int[] {40, 400, 1600, 2000}, output);
+        }
+
+        [Fact]
+        public void given_Unsecure_Uri_Return_false()
+        {
+            //arrange
+            var input = new Uri("http://www.erdetfredag.dk/");
+
+            //act
+            var output = Extensions.isSecure(input);
+
+            //assert
+            Assert.False(output);
+        }
+
+        [Fact]
+        public void given_string_return_3()
+        {
+            //arrange
+            var input = "Hej med dig 123 %%% ?? =! ";
+
+            //act
+            var output = Extensions.WordCount(input);
+
+            //assert
+            Assert.Equal(3, output);
         }
     }
 }
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace BDSA2020.Assignment03
+namespace BDSA2021.Assignment03
 {
     public static class Extensions
     {
+       
+       public static IEnumerable<int> flatten(this IEnumerable<int>[] xs)
+       {
+           return(xs.SelectMany(c => c));
+       }
+
+       public static IEnumerable<int> numbersDivisbleBy7AndGreaterThan42(this int[] ys)
+       {
+            return(ys.Where(c => (c % 7 == 0) && (c > 42)));
+       }
+
+       public static IEnumerable<int> findLeapYears(this int[] ys)
+       {
+           return(ys.Where(c => DateTime.IsLeapYear(c)));
+       }
+
+       public static bool isSecure(this Uri u)
+       {
+           return(u.Scheme == Uri.UriSchemeHttps);
+       }
+
+       public static int WordCount(this string s)
+       {
+           return(s.Split(" ").Where(c => Regex.IsMatch(c, @"([a-z]|[A-Z])")).Count());
+       }
     }
 }
