70ea34c78b6a27eebd1051c18010795018485048
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-09-28 21:08:18 +0200

    Add reverseOrder test

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using System.Linq;
 using BDSA2021.Assignment03;
@@ -46,15 +47,42 @@ namespace BDSA2021.Assignment03.Tests
         }
 
         [Fact]
-        public void reverseOrder_given_wizList()
+        public void reverseOrder_given_wizList_return_JRRR_Tolkien_JK_Rowling_George_Lucas_and_wizards_InDescending()
         {
             //Arrange
-            var wizards = Wizard.Wizards.Value;
+            var sauron = ("J.R.R. Tolkien","Sauron"); 
+            var sirius = ("J. K. Rowling", "Sirius Black");      
+            var severus = ("J. K. Rowling", "Severus Snape");     
+            var rubeus = ("J. K. Rowling", "Rubeus Hagrid");       
+            var ron = ("J. K. Rowling", "Ron Weasly");      
+            var mad = ("J. K. Rowling", "Mad Eye Moody");      
+            var luna = ("J. K. Rowling", "Luna Lovegood");      
+            var lucious = ("J. K. Rowling", "Lucious Malfoy");      
+            var lord = ("J. K. Rowling", "Lord Voldemort");      
+            var hermione = ("J. K. Rowling", "Hermione Granger");      
+            var harry = ("J. K. Rowling", "Harry James Potter");      
+            var ginny = ("J. K. Rowling", "Ginny Weasly");      
+            var draco = ("J. K. Rowling", "Draco Malfoy");      
+            var dobby = ("J. K. Rowling", "Dobby");      
+            var cedric = ("J. K. Rowling", "Cedric Diggory");      
+            var bellatrix = ("J. K. Rowling", "Bellatrix Lestrange");      
+            var albus = ("J. K. Rowling", "Albus Dumbledore");      
+            var darth = ("George Lucas", "Darth Vader");
+
+            var tempExpectedOrder = new[]
+            {
+                sauron, sirius, severus, rubeus, ron, mad, luna, lucious, lord, hermione, harry, ginny, draco, dobby,
+                cedric, bellatrix, albus, darth
+            };
+            IEnumerable<IGrouping<string, string>> expectedOrder =
+                tempExpectedOrder.GroupBy(c => (c.Item1), c => (c.Item2));
+                
 
             //Act
             var groupedWiz = Queries.reverseOrderWizNamesGroupedByCNameThenWName();
 
             //Assert
+            Assert.Equal(expectedOrder, groupedWiz);
         }
 
     }
