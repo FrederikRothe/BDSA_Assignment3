e0762fa3598e6ec03cfcd6a7ae9eb2efef420088
Frederik Rothe <66801011+FrederikRothe@users.noreply.github.com>
2021-09-29 14:17:03 +0200

    Wizard part done. (#4)
    * First extension.
    
    * Namespace fix, and first two extension methods.
    
    * Implent flatten test
    
    * Implemented last Extension method.
    
    * Implement divisibleBy7 test
    
    * Implement leapYears Test
    
    * Last two extension methods.
    
    * Implement Uri test
    
    * Implement workCount test
    
    * Namespace fix.
    
    * reverseString().
    
    * added wizards.
    
    * Last few wizard code implementations done.
    
    * Add reverseOrder test
    
    * LINQ Statements.
    
    * Last LINQ Statement.
    
    Co-authored-by: Thomas <thwr@itu.dk>

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,14 +1,125 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
+using System.Linq;
+using BDSA2021.Assignment03;
 
 namespace BDSA2021.Assignment03.Tests
 {
     public class QueriesTests
     {
         [Fact]
-        public void Test1()
+        public void allWizByRowling_Given_16_rowlingWiz()
         {
+            //Arrange
+            var wizards = Wizard.Wizards.Value;
+            int expectedAmount = 16;
 
+            //Act
+            var rowlingWiz = Queries.allWizByRowling().Count();
+
+            //Assert
+            Assert.Equal(rowlingWiz,expectedAmount);
+        }
+        [Fact]
+        public void allWizByRowlingLINQ_Given_16_rowlingWiz()
+        {
+            //Arrange
+            var wizards = Wizard.Wizards.Value;
+            int expectedAmount = 16;
+
+            //Act
+            var rowlingWiz = Queries.allWizByRowlingLINQ().Count();
+
+            //Assert
+            Assert.Equal(rowlingWiz,expectedAmount);
+        }
+
+        [Fact]
+        public void firstSithLord_given_dart_1977()
+        {
+            //Arrange
+            var wizards = Wizard.Wizards.Value;
+            int? expectedYear = 1977;
+            //Act
+            var firstSithYear = Queries.firstSithLord();
+            //Assert
+            Assert.Equal(firstSithYear,expectedYear);
+        }
+
+        [Fact]
+        public void firstSithLord_given_dart_1977LINQ()
+        {
+            //Arrange
+            var wizards = Wizard.Wizards.Value;
+            int? expectedYear = 1977;
+            //Act
+            var firstSithYear = Queries.firstSithLordLINQ();
+            //Assert
+            Assert.Equal(firstSithYear,expectedYear);
+        }
+
+        [Fact]
+        public void uniqueWiz_given_wizList()
+        {
+            //Arrange
+            var wizards = Wizard.Wizards.Value;
+            var expectedOutput = 16;
+            //Act
+            var uniqeWizses = Queries.uniqueWiz().Count();
+            //Assert
+            Assert.Equal(uniqeWizses,expectedOutput);
+        }
+
+        [Fact]
+        public void uniqueWiz_given_wizListLINQ()
+        {
+            //Arrange
+            var wizards = Wizard.Wizards.Value;
+            var expectedOutput = 16;
+            //Act
+            var uniqeWizses = Queries.uniqueWizLINQ().Count();
+            //Assert
+            Assert.Equal(uniqeWizses,expectedOutput);
+        }
+
+        [Fact]
+        public void reverseOrder_given_wizList_return_JRRR_Tolkien_JK_Rowling_George_Lucas_and_wizards_InDescending()
+        {
+            //Arrange
+            var sauron = ("J.R.R. Tolkien","Sauron"); 
+            var sirius = ("J. K. Rowling", "Sirius Black");      
+            var severus = ("J. K. Rowling", "Severus Snape");     
+            var rubeus = ("J. K. Rowling", "Rubeus Hagrid");       
+            var ron = ("J. K. Rowling", "Ron Weasly");      
+            var mad = ("J. K. Rowling", "Mad Eye Moody");      
+            var luna = ("J. K. Rowling", "Luna Lovegood");      
+            var lucious = ("J. K. Rowling", "Lucious Malfoy");      
+            var lord = ("J. K. Rowling", "Lord Voldemort");      
+            var hermione = ("J. K. Rowling", "Hermione Granger");      
+            var harry = ("J. K. Rowling", "Harry James Potter");      
+            var ginny = ("J. K. Rowling", "Ginny Weasly");      
+            var draco = ("J. K. Rowling", "Draco Malfoy");      
+            var dobby = ("J. K. Rowling", "Dobby");      
+            var cedric = ("J. K. Rowling", "Cedric Diggory");      
+            var bellatrix = ("J. K. Rowling", "Bellatrix Lestrange");      
+            var albus = ("J. K. Rowling", "Albus Dumbledore");      
+            var darth = ("George Lucas", "Darth Vader");
+
+            var tempExpectedOrder = new[]
+            {
+                sauron, sirius, severus, rubeus, ron, mad, luna, lucious, lord, hermione, harry, ginny, draco, dobby,
+                cedric, bellatrix, albus, darth
+            };
+            IEnumerable<IGrouping<string, string>> expectedOrder =
+                tempExpectedOrder.GroupBy(c => (c.Item1), c => (c.Item2));
+                
+
+            //Act
+            var groupedWiz = Queries.reverseOrderWizNamesGroupedByCNameThenWName();
+
+            //Assert
+            Assert.Equal(expectedOrder, groupedWiz);
         }
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace BDSA2021.Assignment03
 {
     public class Queries
     {
+        public static IEnumerable<string> allWizByRowling()
+        {
+            var wizards = Wizard.Wizards.Value;
+            
+            return wizards.Where(c => c.Creator=="J. K. Rowling").Select(c => c.Name);
+        }
+
+        public static IEnumerable<string> allWizByRowlingLINQ()
+        {
+            var wizards = Wizard.Wizards.Value;
+
+            var wizs = from w in wizards
+                        where w.Creator.Contains("J. K. Rowling")
+                        select w.Name;
+
+            return wizs;
+        }
+
+        public static int? firstSithLord()
+        {
+            var wizards = Wizard.Wizards.Value;
+
+            return wizards.Where(c => c.Name.Contains("Darth")).OrderByDescending(c => c.Year).Select(c => c.Year).First();
+        }
+
+        public static int? firstSithLordLINQ()
+        {
+            var wizards = Wizard.Wizards.Value;
+
+            var darth = (from w in wizards
+                        where w.Name.Contains("Darth")
+                        orderby w.Year descending
+                        select w.Year).First();
+            return darth;
+        }
+
+        public static IEnumerable<Tuple<string, int?>> uniqueWiz()
+        {
+            var wizards = Wizard.Wizards.Value;
+
+            return wizards.Where(c => c.Creator=="J. K. Rowling").Select(c => Tuple.Create(c.Name,c.Year));
+        }
+
+        public static IEnumerable<Tuple<string, int?>> uniqueWizLINQ()
+        {
+            var wizards = Wizard.Wizards.Value;
+
+            var unique = from w in wizards
+                        where w.Creator.Contains("J. K. Rowling")
+                        select Tuple.Create(w.Name, w.Year);
+
+            return unique;
+        }
+
+        public static IEnumerable<IGrouping<string,string>> reverseOrderWizNamesGroupedByCNameThenWName()
+        {
+            var wizards = Wizard.Wizards.Value;
+            
+            return wizards.OrderByDescending(c => c.Creator).ThenByDescending(c => c.Name).GroupBy(c => (c.Creator), c => c.Name);
+        }
+
+        public static IEnumerable<string> reverseOrderWizNamesGroupedByCNameThenWNameLINQ()
+        {
+            var wizards = Wizard.Wizards.Value;
+
+            var groups = from w in wizards
+                        orderby w.Creator descending
+                        orderby w.Name descending
+                        group w by new {w.Creator, w.Name} into g
+                        select g.Key.Name;
+            return groups;
+        }
     }
 }
